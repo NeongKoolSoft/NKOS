5b5ab4a4706c1bb64bd3392bb6e90dfd9ab7d0b4
NeongKoolSoft <pke7709@gmail.com>
2026-01-05 17:54:05 +0900

    fix: 260105 1754

--- src/main.jsx
+++ src/main.jsx
@@ -10,16 +10,14 @@ console.log("--- 배포 환경 변수 디버깅 ---");
 console.log("전체 환경변수 객체:", import.meta.env); 
 console.log("GA ID 값:", import.meta.env.VITE_GA_MEASUREMENT_ID);
 
-const GA_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+//const GA_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+const GA_ID = "G-MECV6VXQ1B";
 
 if (GA_ID) {
   ReactGA.initialize(GA_ID);
   console.log("GA4 초기화 완료!");
 } else {
   console.error("GA_ID를 찾을 수 없습니다. 환경변수 설정을 확인하세요.");
-  console.log("--- 배포 환경 변수 디버깅 ---");
-  console.log("전체 환경변수 객체:", import.meta.env); 
-  console.log("GA ID 값:", import.meta.env.VITE_GA_MEASUREMENT_ID);
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
